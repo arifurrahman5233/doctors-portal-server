eac8aad229e6089588588bde0f8f4a5face398c1
Arif <arifurrahman5233>
2021-11-05 14:58:13 +0600

    mongoBD Connect

--- index.js
+++ index.js
@@ -9,9 +9,23 @@ app.use(cors());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.evtnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(uri);
+
+
+async function run (){
+  try{
+    await client.connect();
+    // console.log("data  base connect");
+  }
+  finally{
+
+  }
+
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello Doctors Portal!')
