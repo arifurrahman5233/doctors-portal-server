565a0365c28dbd8c9c8d609ccc9f1f6fb1e6eb5a
Arif <arifurrahman5233>
2021-11-18 12:04:06 +0600

    Secure Admin

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run (){
     await client.connect();
     const database =client.db('doctors_portal');
     const appointmentsCollection = database.collection('appointments');
+    const usersCollection = database.collection('user');
 
 //----Data Get---//
 
@@ -44,6 +45,23 @@ async function run (){
       res.json(result)
     });
 
+      app.post('/users', async(req , res) => {
+        const user = req.body;
+        const result = await usersCollection.insertOne(user);
+        console.log(result);
+        res.json(result);
+      });
+
+//------Data put------//
+
+      app.put('/users', async(req, res) => {
+        const user = req.body;
+        const filter = {email: user.email};
+        const options = { upsert: true };
+        const updateDoc = { $set: user};
+        const result = await usersCollection.updateOne(filter , updateDoc, options);
+        res.json(result);
+      })
 
   }
   finally{
